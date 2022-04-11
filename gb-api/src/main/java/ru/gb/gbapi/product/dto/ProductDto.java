c54d17fd9c2bc3db0c1187ee7175dd5e54b01838
PSelyutin <pselyutin@inno.tech>
2022-04-11 09:00:30 +0400

    lesson3 change ProductDto in gb-api

--- gb-api/src/main/java/ru/gb/gbapi/product/dto/ProductDto.java
+++ gb-api/src/main/java/ru/gb/gbapi/product/dto/ProductDto.java
@@ -1,12 +1,21 @@
 package ru.gb.gbapi.product.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import ru.gb.gbapi.common.enums.Status;
 
-import javax.validation.constraints.*;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 @Getter
 @Setter
@@ -19,12 +28,13 @@ public class ProductDto {
     private String title;
     @NotNull
     @DecimalMin(value = "0.0", inclusive = false)
-    @Digits(integer=3, fraction=2)
+    @Digits(integer = 3, fraction = 2)
     private BigDecimal cost;
     @PastOrPresent
-    @JsonFormat(pattern="dd.MM.yyyy")
+    @JsonFormat(pattern = "dd.MM.yyyy")
     private LocalDate manufactureDate;
     @NotNull
     private Status status;
     private String manufacturer;
+    private List<String> category;
 }
